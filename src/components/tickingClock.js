38637864e8680fb90d9919062a1fab2e1e7f8bc3
itsrealoj <stephenojwang1040@gmail.com>
2020-04-02 15:16:39 +0300

    clock ticking now

--- src/components/tickingClock.js
+++ src/components/tickingClock.js
@@ -4,17 +4,30 @@ import React, { Component } from 'react';
 class TickingClock extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
-            date: Date(),
+            date: new Date().toLocaleString(),
         }
     }
+
+    componentDidMount() {
+        this.intervalID = setInterval(() => this.tick(), 1000)
+    }
+    componentWillMount() {
+        clearInterval(this.intervalID);
+    }
+
+    tick(){
+        this.setState({
+            date: new Date().toLocaleString()
+        })
+    }
+
     render() {
         return(
             <div>
                 <h1>{this.state.date}</h1>
             </div>
-        )
+        );
     }
 }
 
