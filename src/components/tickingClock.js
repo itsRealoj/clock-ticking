57978d65832d531442066662f13638697f6ba444
itsrealoj <stephenojwang1040@gmail.com>
2020-04-02 10:22:09 +0300

    initialized a ticking clock component

---
+++ src/components/tickingClock.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class TickingClock extends Component {
+    render() {
+        return(
+            <div>
+                <h1>My clock!</h1>
+            </div>
+        )
+    }
+}
+
+export default TickingClock;
