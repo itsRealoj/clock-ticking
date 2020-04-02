19408450a198dd12f415a7258d5298fdb75f2fe9
itsrealoj <stephenojwang1040@gmail.com>
2020-04-02 10:33:48 +0300

    passing dates through props

--- src/components/tickingClock.js
+++ src/components/tickingClock.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 
 
 class TickingClock extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            date: Date(),
+        }
+    }
     render() {
         return(
             <div>
-                <h1>{Date()}</h1>
+                <h1>{this.state.date}</h1>
             </div>
         )
     }
